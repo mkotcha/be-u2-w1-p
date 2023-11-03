896f384107ccefa5c8b00c1ee4f3abfdad111595
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-03 17:12:52 +0100

    removed hibernate warning EqualsAndHashCode

--- src/main/java/org/emmek/beu2w1p/entities/ReservationId.java
+++ src/main/java/org/emmek/beu2w1p/entities/ReservationId.java
@@ -1,13 +1,16 @@
 package org.emmek.beu2w1p.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@EqualsAndHashCode
 public class ReservationId implements Serializable {
     private User user;
     private LocalDate date;
+
 
 }
