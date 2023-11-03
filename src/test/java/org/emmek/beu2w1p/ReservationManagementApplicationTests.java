aea972173405976c3cf4bcdf4587a66c362ea9b1
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-03 13:32:41 +0100

    first commit

---
+++ src/test/java/org/emmek/beu2w1p/ReservationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w1p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
